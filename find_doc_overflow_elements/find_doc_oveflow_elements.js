b554cdadf7a31885052966807281f187925e3b63
jaimegsandoval <jaime.devs@gmail.com>
2022-02-27 20:17:38 -0800

    Add doc overflow function

---
+++ find_doc_overflow_elements/find_doc_oveflow_elements.js
@@ -0,0 +1,6 @@
+const docWidth = document.documentElement.offsetWidth;
+[].forEach.call(document.querySelectorAll('*'), function (el) {
+  if (el.offsetWidth > docWidth) {
+    console.log(el);
+  }
+});
